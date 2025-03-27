337c1543b899e01709596014ac440694e717e88d
EliSullivan <easullivan@mail.lipscomb.edu>
2025-03-27 12:26:15 -0500

    removed id from playlist

--- server/models/playlist.js
+++ server/models/playlist.js
@@ -4,11 +4,6 @@ import sequelize from '../config/database.js';
 
 const Playlist = sequelize.define('playlist', {
   title: DataTypes.STRING,
-<<<<<<< HEAD
-  id: DataTypes.INTEGER,
-=======
-  id: DataTypes.INT,
->>>>>>> refs/remotes/origin/main
 });
 
 Playlist.prototype.toJSON = function() {

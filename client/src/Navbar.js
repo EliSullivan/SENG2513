487fae9424226f9dcd52215e0668dde7117c6ce1
Trinity <tgmorris@mail.lipscomb.edu>
2025-04-07 22:03:31 -0500

    Song playlist table created.

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -4,7 +4,7 @@ import React from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Companies from "./Companies";
-import Users from "./Users";
+//import Users from "./Users";
 import Songs from "./Songs";
 const Navbar = () => {
     return (

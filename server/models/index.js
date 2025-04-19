68bf48817cba31a12488d9277d4b005d0ca3a40c
EliSullivan <easullivan@mail.lipscomb.edu>
2025-04-19 13:17:39 -0500

    Fixed issues with the model index.js

--- server/models/index.js
+++ server/models/index.js
@@ -10,7 +10,7 @@ let detailsIds = "";
 let searchTerm = "";
 
 const apiHeaders = {
-  'x-rapidapi-key': 'c0d9a61a11msh4bc465a39dbbd8ep110b0fjsn9d9f22401ae0',
+  'x-rapidapi-key': '5cb5d989efmsh8ebf3b013958e2ap16a7e7jsnb61418d48af4',
   'x-rapidapi-host': 'spotify-downloader9.p.rapidapi.com'
 }
 
@@ -19,7 +19,8 @@ const songDetailsOptions = {
   url: 'https://spotify-downloader9.p.rapidapi.com/tracks',
   params: {
     ids: detailsIds,
-  }
+  },
+  headers: apiHeaders
 }
 
 const searchResultsOptions = {
@@ -32,7 +33,7 @@ const searchResultsOptions = {
 }
 
 
-export async function fetchData() {
+export async function fetchData(options) {
   try {
     const response = await axios.request(options);
     console.log(response.data);
@@ -64,38 +65,44 @@ const syncModels = async () => {
     await sequelize.sync({ alter: true });
     console.log('All models were synchronized successfully.');
 
-    // when not hard coded, don't fetchData everytime models sync
-    // if(songDetailsUpToDate)
-    apiSongDetails = await fetchData(songDetailsOptions);
-    const tracks = apiSongDetails.data?.tracks;
+    // Only fetch data if we have IDs to query
+    if (detailsIds) {
+      const apiSongDetails = await fetchData(songDetailsOptions);
+      const tracks = apiSongDetails?.data?.tracks || [];
 
-    const processedTracks = tracks.map(track => {
-      const trackData = {
-        id: track.id,
-        title: track.name,
-        runtime: track.duration_ms,
-        artist: track.artists.map(artist => artist.name).join(', '),
-        album: track.album.name,
-        albumCoverUrl: track.album.images[0]?.url || null,
-        previewUrl: track.preview_url || null
-      };
-      return trackData;
-    });
-    
-    // when not hard coded, don't fetchData everytime models sync
-    // if (searchResultsUpToDate())
-    searchResults = await fetchData(searchResultsOptions);
-    const trackResults = searchResults.data?.tracks;
+      const processedTracks = tracks.map(track => {
+        const trackData = {
+          id: track.id,
+          title: track.name,
+          runtime: track.duration_ms,
+          artist: track.artists.map(artist => artist.name).join(', '),
+          album: track.album.name,
+          albumCoverUrl: track.album.images[0]?.url || null,
+          previewUrl: track.preview_url || null
+        };
+        return trackData;
+      });
 
-    const processedSearchResults = trackResults?.items.map(track => {
-      const trackData = {
-        id: track.id,
-        title: track.name,
-        artists: track.artists.join(', '),
-        runtime: track.duration
-      };
-      return trackData;
-    });
+      await Song.bulkCreate(processedTracks);
+    }
+
+    // Only fetch search results if we have a search term
+    if (searchTerm) {
+      const searchResults = await fetchData(searchResultsOptions);
+      const trackResults = searchResults?.data?.tracks?.items || [];
+
+      const processedSearchResults = trackResults.map(track => {
+        const trackData = {
+          id: track.id,
+          title: track.name,
+          artists: track.artists.join(', '),
+          runtime: track.duration
+        };
+        return trackData;
+      });
+
+      await SearchResults.bulkCreate(processedSearchResults);
+    }
 
     // Generate 10 users
     const users = [];
@@ -110,17 +117,13 @@ const syncModels = async () => {
     for (let i = 1; i <= 10; i++) {
       playlist.push({
         title: `Playlist ${i}`,
-        //songs.push()
       });
     }
 
     // Insert all data in parallel
     await Promise.all([
       Playlist.bulkCreate(playlist),
-      Song.bulkCreate(processedTracks),
       User.bulkCreate(users),
-      SearchResults.bulkCreate(searchResults)
-      
     ]);
     
     console.log('All data inserted successfully.');

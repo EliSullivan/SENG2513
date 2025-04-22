ac654ebe5f032a78cab83bcf7918501f265f2238
kjharalson <kjharalson@mail.lipscomb.edu>
2025-04-22 12:34:09 -0500

    made search results scroll

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import "./Search.css";
 
 const Search = () => {
   const [searchResults, setSearchResults] = useState([]);
